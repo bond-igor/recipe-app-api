7b7f91555ef994b4cfa5675cb7f766b25095a0de
Igor Bond <npro27@yandex.ru>
2020-11-29 17:19:34 +1000

    Configured postgres db

--- app/core/admin.py
+++ app/core/admin.py
@@ -16,7 +16,7 @@ class UserAdmin(BaseUserAdmin):
         }),
         (_('Personal Info'), {'fields': ('name',)}),
         (
-            _('Permissions'), 
+            _('Permissions'),
             {'fields': ('is_active', 'is_staff', 'is_superuser')}
         ),
         (_('Important dates'), {'fields': ('last_login',)})
